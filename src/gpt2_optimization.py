7e91d878854dcfa3449e243ee0454db2ffd348c0
SouLeo <slwanna@utexas.edu>
2023-01-07 10:57:56 -0700

    gpt2 logprob and decoding working

--- src/gpt2_optimization.py
+++ src/gpt2_optimization.py
@@ -1,7 +1,7 @@
 import torch
 from torch.utils.data import DataLoader
 import transformers
-from transformers import GPT2Tokenizer, GPT2Model
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
 
 from umrf_dataset import UMRF
 
@@ -22,4 +22,20 @@ if __name__ == '__main__':
     device = check_for_gpu()
 
     gpt2_tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-    gpt2_model = GPT2Model.from_pretrained("gpt2").to(device)
+    gpt2_model = GPT2LMHeadModel.from_pretrained("gpt2").to(device)
+
+    prompt = "hello my name is Selma"
+    prompt_tokenized = gpt2_tokenizer(prompt, return_tensors="pt").to(device)
+    outputs = gpt2_model.generate(**prompt_tokenized, return_dict_in_generate=True,
+     output_scores=True, max_new_tokens=100)
+    
+    # output_scores provide logits over GPT2 vocabulary size
+    # to make into logprobs, apply torch.softmax()
+    output_scores = outputs['scores']
+
+    # greedy decoding of most likely next token prediction
+    output_tokens = gpt2_tokenizer.decode(outputs['sequences'][0])
+
+
+
+
